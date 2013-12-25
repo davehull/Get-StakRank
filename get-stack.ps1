6ad2219eee1c51ec8d1d8e25a4206e64c68bd060
Dave Hull <dpgit@trustedsignal.com>
2013-12-25 12:27:10 -0800

    Update get-stack.ps1
    Added Get-Fields.ps1 reference.

--- get-stack.ps1
+++ get-stack.ps1
@@ -7,6 +7,10 @@ as with import-csv, if not specified csv is assumed with the first row assumed t
 The user specifies the fields by which to stack the data, defaulting in ascending order, creating a 
 table where less frequently occuring items bubble up, if mutliple fields are provided as an argument,
 those fields in combination will be paired or tupled.
+
+If you don't know the fields and you're frequently working with various separated values files, 
+https://github.com/davehull/Get-Fields.ps1, may be useful.
+
 .PARAMETER Path
 Specifies the path to the separated values file.
 .PARAMETER LiteralPath
@@ -25,6 +29,8 @@ Data should be sorted by the key.
 Data should be sorted by the value.
 .PARAMETER Fields
 Specifies the field or fields to rank.
+.Parameter ShowFields
+Causes the script to return the field names.
 .EXAMPLE
 Get-Stack -Path .\autouns.tsv -delimiter "`t" -Asc -Key
 #>
@@ -47,66 +53,19 @@ Param(
         [array]$fields
 )
 
-function Get-Data {
-<#
-.SYNOPSIS
-Open delimite file, read into variable, return variable
-.PARAMETER Path
-Specifies the full path to the input file
-.PARAMETER Delimiter
-Specifies the delimiter for the input file
-.PARAMETER Header
-Specifies the header for the input file, if none, first line of file is used
-#>
-Param(
-    [Parameter(ParameterSetname='path',Mandatory=$True,Position=0)]
-        [string]$Path,
-    [Parameter(ParameterSetname='litpath',Mandatory=$True,Position=0)]
-        [string]$LiteralPath,
-    [Parameter(Mandatory=$False,Position=1)]
-        [char]$Delimiter=",",
-    [Parameter(Mandatory=$False,Position=2)]
-        [string]$Header
-)
-    if ($Header.Length -gt 0) {
-        try {
-            import-csv -Path $Path -Delimiter $Delimiter -Header $Header
-        } catch {}
-    }
-
-
-}
-
 switch ($PSCmdlet.ParameterSetName) {
     Path { 
         if ($Header.Length -gt 0) {
-            $data = get-data -Path $Path -Delimiter $Delimiter -Header $Header
+            $Data = Import-Csv -Path $Path -Delimiter $Delimiter -Header $Header
         } else {
-            $data = get-data -Path $Path -Delimiter $Delimiter
+            $Data = Import-Csv -Path $Path -Delimiter $Delimiter
         }
     }
     LitPath {
-        if ($header.Length -gt 0) {
-            $data = get-data -Path $Path -Delimiter $Delimiter -Header $Header
+        if ($Header.Length -gt 0) {
+            $Data = Import-Csv -LiteralPath $Path -Delimiter $Delimiter -Header $Header
         } else {
-            $data = get-data -Path $Path -Delimiter $Delimiter
+            $Data = Import-Csv -LiteralPath $Path -Delimiter $Delimiter
         }
     }
-
-<# 
-$data = import-csv -delimiter "`t" $file
-
-$stack = @{}
-
-$data | ? { $_.FailureReason -eq "" } | % { $fieldValue = $_.$field + "`t" + $_.LogonType + "`t" +$_.SubjectUserName
-    if ($stack.containskey($fieldValue)) {
-        $stack.set_item($fieldValue, $stack.get_item($fieldValue) + 1)
-    } else {
-        $stack.add($fieldValue, 1)
-    }
 }
-
-$data_out = $stack.GetEnumerator() | sort-object value,key | % {[string]$_.value + "`t" + $_.key + "`r`n"}
-
-$data_out.trim()
-#>
